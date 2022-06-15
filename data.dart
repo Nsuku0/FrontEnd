90a73d613ed04e2ca201a23adce8683894100d6d
Rinya <rinyasingh@gmail.com>
2022-06-15 11:53:59 +0200

    data.dart, excel reading code added, not working

--- data.dart
+++ data.dart
@@ -1,5 +1,21 @@
 import 'package:FrontEnd/constants.dart';
 import 'package:FrontEnd/models/company_info_model.dart';
+import 'package:excel/excel.dart';
+import 'package:flutter/services.dart' show ByteData, rootBundle;
+
+void readExcelFile() {
+  ByteData data = await rootBundle.load("assets/companyData.xlsx");
+  var bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+  var excel = Excel.decodeBytes(bytes);
+  int j =0;
+  int row = 18;
+  int col = 2;
+  var listVar = List.generate(row, (i) => List(col), growable:false);
+  for (var table in excel.tables.keys) {
+    //TODO: add excel code
+  }
+}
+
 
 List companyData = [
   CompInfo (
