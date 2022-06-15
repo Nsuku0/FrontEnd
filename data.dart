893726da3384f67ce0f889a04699049a8eb565b1
Rinya <rinyasingh@gmail.com>
2022-06-15 10:53:36 +0200

    pubspec updated with excel dependency

--- data.dart
+++ data.dart
@@ -1,5 +1,12 @@
 import 'package:FrontEnd/constants.dart';
+import 'package:FrontEnd/models/company_info_model.dart';
 
 List companyData = [
-  
+  CompInfo (
+    name: 'C1',
+    sector: 'x',
+    iconSrc: 'assets/C1.png',
+    revenue: 89384.93,
+    carbonEmissions: 288373.0339,
+  )
 ]
--- models/company_info_model.dart
+++ models/company_info_model.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 class CompInfo {
-  final String? name, sector;
+  final String? name, sector, iconSrc;
   final double? revenue, carbonEmissions;
   CompInfo({
     this.name,
     this.sector,
+    this.iconSrc,
     this.revenue,
     this.carbonEmissions,
   });
