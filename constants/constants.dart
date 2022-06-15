d8cb7da2c1396f22a44e867e11424b999e85ccbd
Rinya <rinyasingh@gmail.com>
2022-06-15 11:07:41 +0200

    Color issue

--- constants/constants.dart
+++ constants/constants.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 
 const bg = Color.fromRGBO(245, 45, 40, 1); //agile
-const panels = Colors.white;
-const headings_orange = Color.fromRGBO(250, 85, 30, 1); //prepared
-const text_col = Colors.black;
+const panels = white;
+const headings_orange = Colors.fromRGBO(250, 85, 30, 1); //prepared
+const text_col = Color.black;
 const lightText = Colors.black26;
 
-const List <Color> pinks = [Color.fromRGBO(135, 19, 60, 1), 
-                              Color.fromRGBO(175, 20, 75, 1), 
-                                Color.fromRGBO(240, 50, 90, 1), 
-                                  Color.fromRGBO(240, 90, 120, 1)];
-const List <Color> oranges = [Color.fromRGBO(225, 120, 15, 1), 
-                                Color.fromRGBO(250, 85, 30, 1), 
-                                  Color.fromRGBO(245, 45, 40, 1)];                                
+List<Colors> pinks = [
+  Colors.fromRGBO(135, 19, 60, 1),
+  Colors.fromRGBO(175, 20, 75, 1),
+  Colors.fromRGBO(240, 50, 90, 1),
+  Colors.fromRGBO(240, 90, 120, 1)
+];
+List<Colors> oranges = [
+  Colors.fromRGBO(225, 120, 15, 1),
+  Colors.fromRGBO(250, 85, 30, 1),
+  Colors.fromRGBO(245, 45, 40, 1)
+];
 
 const appPadding = 15.0;
