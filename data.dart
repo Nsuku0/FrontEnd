2e4bc704ceec31104fe41f0299e8719b0b1582a1
Rinya <rinyasingh@gmail.com>
2022-06-20 22:08:35 +0200

    company info

--- data.dart
+++ data.dart
@@ -1,26 +1,31 @@
-import 'constants/constants.dart';
 import 'models/company_info_model.dart';
 import 'package:flutter/services.dart' show ByteData, rootBundle;
-import 'dart:io';
+import 'dart:core';
 import 'package:path/path.dart' as p;
-List<String> data = [];
 
-void readFileByLines(){
-  File file = new File('./assets/CompData.xlsx');
-  List<String> lines = file.readAsLinesSync();
-  data = lines.sublist(1,lines.length);
+import 'package:excel/excel.dart';
+
+List<CompInfo> companies = [];
+
+ByteData? data = rootBundle.load("assets/CompanyData.xlsx") as ByteData?;
+var bytes = data!.buffer.asUint8List(data!.offsetInBytes, data?.lengthInBytes);
+var excel = Excel.decodeBytes(bytes);
+read() {
+  for (var table in excel.tables.keys) {
+    var c;
+    for (var row in excel.tables[table]!.rows) {
+      // ignore: avoid_print
+      companies.add(CompInfo(
+        name: row[0],
+        sector: row[1],
+        scope1_2021: row[2],
+        scope2_2021: row[3],
+        rev_2021: row[4],
+        scope1_2020: row[5],
+        scope2_2020: row[6],
+        rev2020: row[7],
+        iconSrc: row[8],
+      ));
+    }
+  }
 }
-
-String getCompanyName(int idx) {
-  return data[0;, idx];
-}
-
-List companyData = [
-  CompInfo (
-    name: 'C1',
-    sector: 'x',
-    iconSrc: 'assets/C1.png',
-    revenue: 89384.93,
-    carbonEmissions: 288373.0339,
-  )
-]
--- models/company_info_model.dart
+++ models/company_info_model.dart
@@ -2,12 +2,21 @@ import 'package:flutter/material.dart';
 
 class CompInfo {
   final String? name, sector, iconSrc;
-  final double? revenue, carbonEmissions;
+  final double? rev_2021,
+      rev2020,
+      scope1_2021,
+      scope2_2021,
+      scope1_2020,
+      scope2_2020;
   CompInfo({
     this.name,
     this.sector,
     this.iconSrc,
-    this.revenue,
-    this.carbonEmissions,
+    this.rev_2021,
+    this.rev2020,
+    this.scope1_2021,
+    this.scope2_2021,
+    this.scope1_2020,
+    this.scope2_2020,
   });
 }
