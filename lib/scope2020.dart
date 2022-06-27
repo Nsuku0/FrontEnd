2276d8b904bd8377189cb319eee2cd770e708a27
Nsuku Minyuku <minyukunsuku0@gmail.com>
2022-06-27 03:38:22 +0200

    Update scope2020.dart

--- lib/scope2020.dart
+++ lib/scope2020.dart
@@ -1,71 +1,51 @@
-//import 'package:charts_flutter/flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'constants/constants.dart';
 
 
-class MyApp extends StatefulWidget{
+class Scope2020 extends StatelessWidget{
 
-    const MyApp({Key? key}) : super(key: key);
-    State createState() => App();
-}
+   Scope2020 ({Key? key}) : super(key: key);
 
-  class App extends State<MyApp>{
-    late List<RevDat> _chartData;  // initializes chart data field
+  final List<RevDat> chartData = [
+    RevDat("Retail", 487172,pinks[0]),
+    RevDat("Mining", 1049767,headings_orange),
+    RevDat("Manu", 74415, pinks[2]),
+    RevDat("Agri", 130559,oranges[0]),
+    RevDat("TMT", 386541, lightText),
+  ];
 
-    @override
-    void initState(){
-      _chartData = getSectorInfo();
-      super.initState();
-    }
-
-    @override
-    Widget build(BuildContext context){
-      return SafeArea(
+  @override
+  Widget build(BuildContext context){
+    return SafeArea(
         child: Scaffold(
-          body: SfCircularChart(
-    title: 
-         ChartTitle(text:"Combined Scope 1 and 2 Emissions per Sector 2020",//returns titlte on piechart
-              backgroundColor: Colors.white,
-                  borderColor: Colors.white,
-                  borderWidth: 2,
-                  // Aligns the chart title to left
-                  alignment: ChartAlignment.near,
+            body: SfCircularChart(
+                title:
+                ChartTitle(text:"Scope Emissions 2020" ,//returns titlte on piechart
+
                   textStyle: TextStyle(
-                    color: oranges[3], //colour of title
+                    color: oranges[2], //colour of title
                     fontFamily: 'Calibri',    // Sets title font
                     fontStyle: FontStyle.normal,
                     fontWeight: FontWeight.bold, //Make Title bold
                     fontSize: 18,), // Size of title),  //returns titlte on piechart
-         ),
-    legend: 
-         Legend(
-          position: LegendPosition.right, // positioning of key
-          isVisible: true, 
-          isResponsive: true,
-          overflowMode: LegendItemOverflowMode.wrap), //returns key/legend on piechart
-        series: <CircularSeries>[
-        PieSeries<RevDat, String>(
-        dataSource: _chartData,
-        xValueMapper: (RevDat data,_) => data.sector,
-        yValueMapper: (RevDat data,_) => data.emission,
-        dataLabelSettings: DataLabelSettings(isVisible: true))
-      ])));         // returns chart in usable area
-    }
-    
+                ),
+                legend:
+                Legend(
+                    position: LegendPosition.right, // positioning of key
+                    isVisible: true,
+                    isResponsive: false,
+                    overflowMode: LegendItemOverflowMode.wrap),  //returns key/legend on piechart
+                series: <CircularSeries>[
+                  PieSeries<RevDat, String>(
+                      dataSource: chartData,
+                      xValueMapper: (RevDat chartData,_) => chartData.sector,
+                      yValueMapper: (RevDat chartData,_) => chartData.emission,
+                      pointColorMapper:(RevDat chartData,  _) => chartData.color,
+                      dataLabelSettings: const DataLabelSettings(isVisible: true))
+                ])));         // returns chart in usable area
   }
 
-  List<RevDat> getSectorInfo(){
-    final List<RevDat> chartData = [
-      RevDat("Retail", 487172.33,Color.fromRGBO(135, 19, 60, 1) ),
-      RevDat("Mining", 1049766.5,Color.fromRGBO(175, 20, 75, 1)),
-      RevDat("Banking", 306420.2096,Color.fromRGBO(240, 50, 90, 1) ),
-      RevDat("Manufactoring", 74415.33, Color.fromRGBO(240, 90, 120, 1)),
-      RevDat("Agriculture", 130558.67,Color.fromRGBO(225, 120, 15, 1) ),
-      RevDat("TMT", 386540.5, Color.fromRGBO(250, 85, 30, 1)),
-    ];
-    return chartData;
-  
 }
 
 class RevDat{
