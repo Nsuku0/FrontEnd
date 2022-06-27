f8f7866135061797cb02633437b9f668ff8a3cb7
Nsuku Minyuku <minyukunsuku0@gmail.com>
2022-06-27 03:40:01 +0200

    charts uses selected colors

--- lib/avgrev.dart
+++ lib/avgrev.dart
@@ -1,68 +1,68 @@
-//import 'package:charts_flutter/flutter.dart';
-//import 'package:charts_flutter/flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'constants/constants.dart';
 
 
-class MyApp extends StatefulWidget{
+class Average extends StatefulWidget{
 
-    const MyApp({Key? key}) : super(key: key);
-    State createState() => App();
-    
+  const Average({Key? key}) : super(key: key);
+  @override
+  State createState() => App();
+
 }
-  class App extends State<MyApp>{
-    late List<RevDat> _chartData;  // initializes chart data field
-    @override
-    void initState(){
-      _chartData = getSectorInfo();
-      super.initState();
-    }
+class App extends State<Average>{
+  late List<RevDat> _chartData;  // initializes chart data field
+  @override
+  void initState(){
+    _chartData = getSectorInfo();
+    super.initState();
+  }
 
-    @override
-    Widget build(BuildContext context){
-      return SafeArea(
+  @override
+  Widget build(BuildContext context){
+    return SafeArea(
         child: Scaffold(
-          body: SfCircularChart(
-      title: 
-              ChartTitle(text:"Average Revenue Per Sector",
-              backgroundColor: Colors.white,
+            body: SfCircularChart(
+                title:
+                ChartTitle(text:"Average Revenue Per Sector",
+                  backgroundColor: Colors.white,
                   borderColor: Colors.white,
                   borderWidth: 2,
                   // Aligns the chart title to left
                   alignment: ChartAlignment.near,
                   textStyle: TextStyle(
-                    color: oranges[3], //colour of title
+                    color: oranges[2], //colour of title
                     fontFamily: 'Calibri',    // Sets title font
                     fontStyle: FontStyle.normal,
                     fontWeight: FontWeight.bold, //Make Title bold
                     fontSize: 18,), // Size of title
-              ),  //returns titlte on piechart
-      legend: 
-              Legend(
-              position:LegendPosition.right, // positioning of key  
-              isVisible: true, 
-              isResponsive: true, ///makes key interactive
-              overflowMode: LegendItemOverflowMode.wrap), //returns key/legend on piechart  
-            series: <CircularSeries>[ 
-              PieSeries<RevDat, String>(
-              dataSource: _chartData,
-              xValueMapper: (RevDat data,_) => data.sector,
-              yValueMapper: (RevDat data,_) => data.revenue,
-              )
-      ])));        // returns chart in usable area
-    }
-    
-  
+                ),  //returns titlte on piechart
+                legend:
+                Legend(
+                    position:LegendPosition.right, // positioning of key
+                    isVisible: true,
+                    isResponsive: false, ///makes key interactive
+                    overflowMode: LegendItemOverflowMode.wrap), //returns key/legend on piechart
+                series: <CircularSeries>[
+                  PieSeries<RevDat, String>(
+                    dataSource: _chartData,
+                    xValueMapper: (RevDat data,_) => data.sector,
+                    yValueMapper: (RevDat data,_) => data.revenue,
+                      pointColorMapper:(RevDat chartData,  _) => chartData.color,
+                      dataLabelSettings: const DataLabelSettings(isVisible: true)
+                  )
+                ])));        // returns chart in usable area
+  }
+
+
 
   List<RevDat> getSectorInfo(){
     final List<RevDat> chartData = [
-      RevDat("Retail",123472.9, Color.fromRGBO(135, 19, 60, 1),),
-      RevDat("Mining", 262915, Color.fromRGBO(175, 20, 75, 1)),
-      RevDat("Banking",95156.5, Color.fromRGBO(240, 50, 90, 1) ),
-      RevDat("Manufactoring",81221.76,Color.fromRGBO(240, 90, 120, 1) ),
-      RevDat("Agriculture", 72134.04,Color.fromRGBO(225, 120, 15, 1)),
-      RevDat("TMT",45733.32,Color.fromRGBO(250, 85, 30, 1)),
+      RevDat("Retail",123473, pinks[0]),
+      RevDat("Mining", 262915,headings_orange),
+      RevDat("Manu",81222,pinks[2]),
+      RevDat("Agri", 72134,oranges[0]),
+      RevDat("TMT",45733,lightText),
     ];
     return chartData;
   }
